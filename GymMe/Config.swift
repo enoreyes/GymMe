9dd7f4b63aae1124485f58928aaba9c64791fe8a
Eno Reyes <enoreyes@gmail.com>
2017-10-18 23:12:03 -0400

    Update Config.swift

--- GymMe/Config.swift
+++ GymMe/Config.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Config {
-    static var STORAGE_ROOT_REF = "gs://gymme-4f5ba.appspot.com"
+    static var STORAGE_ROOT_REF = ""
     static var GYM_RED = UIColor(red: 237/255, green: 28/255, blue: 39/255, alpha: 1.0)
     static var GYM_GRADIENT = UIColor(red: 239/255, green: 71/255, blue: 58/255, alpha: 1.0)
 
